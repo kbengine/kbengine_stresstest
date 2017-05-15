476826bd29d355c3b87dd901924805fcffaf1ebb
kbengine <3603661@qq.com>
2017-05-15 09:43:33 +0800

    up

--- print_servers.sh
+++ print_servers.sh
@@ -4,4 +4,4 @@ export KBE_ROOT="$(pwd | awk -F "/kbengine/" '{print $1}')/kbengine"
 export KBE_RES_PATH="$KBE_ROOT/kbe/res/:$(pwd):$(pwd)/res:$(pwd)/scripts/"
 export KBE_BIN_PATH="$KBE_ROOT/kbe/bin/server/"
 
-python _watchlog.py&
+python $KBE_ROOT/kbe/tools/server/pycluster/cluster_controller.py
