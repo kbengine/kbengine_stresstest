22da7791e4de233acd3d481457fd310ade39f13b
kebiao <380000937@qq.com>
2018-07-27 11:35:50 +0800

    up

--- print_servers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-export KBE_ROOT="$(pwd | awk -F "/kbengine/" '{print $1}')/kbengine"
-export KBE_RES_PATH="$KBE_ROOT/kbe/res/:$(pwd):$(pwd)/res:$(pwd)/scripts/"
-export KBE_BIN_PATH="$KBE_ROOT/kbe/bin/server/"
-
-python $KBE_ROOT/kbe/tools/server/pycluster/cluster_controller.py
