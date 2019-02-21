ea9a43922dfa4f454166037067bac2cc7a26ce31
kebiao <380000937@qq.com>
2019-02-21 11:27:23 +0800

    up

--- mmorpg/server_assets/start_server.sh
+++ mmorpg/server_assets/start_server.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 
-export KBE_ROOT="$(pwd | awk -F "/kbengine/" '{print $1}')/kbengine"
+currPath=$(pwd)
+keyStr="/kbengine/"
+
+bcontain=`echo $currPath|grep $keyStr|wc -l`
+
+
+if [ $bcontain = 0 ]
+then
+	export KBE_ROOT=$(cd ../; pwd)
+else
+	export KBE_ROOT="$(pwd | awk -F "/kbengine/" '{print $1}')/kbengine"
+fi
+
+
+
 export KBE_RES_PATH="$KBE_ROOT/kbe/res/:$(pwd):$(pwd)/res:$(pwd)/scripts/"
 export KBE_BIN_PATH="$KBE_ROOT/kbe/bin/server/"
 
@@ -16,24 +30,6 @@ $KBE_BIN_PATH/interfaces --cid=1129652375332859700 --gus=3&
 $KBE_BIN_PATH/dbmgr --cid=3129652375332859700 --gus=4&
 $KBE_BIN_PATH/baseappmgr --cid=4129652375332859700 --gus=5&
 $KBE_BIN_PATH/cellappmgr --cid=5129652375332859700 --gus=6&
-
-$KBE_BIN_PATH/baseapp --cid=6129652375332859701 --gus=7&
-$KBE_BIN_PATH/baseapp --cid=6129652375332859702 --gus=8&
-$KBE_BIN_PATH/baseapp --cid=6129652375332859703 --gus=9&
-$KBE_BIN_PATH/baseapp --cid=6129652375332859704 --gus=10&
-$KBE_BIN_PATH/baseapp --cid=6129652375332859705 --gus=11&
-$KBE_BIN_PATH/baseapp --cid=6129652375332859706 --gus=12&
-$KBE_BIN_PATH/baseapp --cid=6129652375332859707 --gus=13&
-$KBE_BIN_PATH/baseapp --cid=6129652375332859708 --gus=14&
-
-$KBE_BIN_PATH/cellapp --cid=7129652375332859701 --gus=15&
-$KBE_BIN_PATH/cellapp --cid=7129652375332859702 --gus=16&
-$KBE_BIN_PATH/cellapp --cid=7129652375332859703 --gus=17&
-$KBE_BIN_PATH/cellapp --cid=7129652375332859704 --gus=18&
-$KBE_BIN_PATH/cellapp --cid=7129652375332859705 --gus=19&
-$KBE_BIN_PATH/cellapp --cid=7129652375332859706 --gus=20&
-$KBE_BIN_PATH/cellapp --cid=7129652375332859707 --gus=21&
-$KBE_BIN_PATH/cellapp --cid=7129652375332859708 --gus=22&
-
-$KBE_BIN_PATH/loginapp --cid=8129652375332859700 --gus=23&
-
+$KBE_BIN_PATH/baseapp --cid=6129652375332859700 --gus=7&
+$KBE_BIN_PATH/cellapp --cid=7129652375332859700 --gus=8&
+$KBE_BIN_PATH/loginapp --cid=8129652375332859700 --gus=9&
